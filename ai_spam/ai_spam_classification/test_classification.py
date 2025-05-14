e51b9ca240f417c95afae97894e9181af83cb2a3
bayana <122169908+BkBayana@users.noreply.github.com>
2025-05-14 20:05:16 +0600

    Delete ai_spam/ai_spam_classification directory

--- ai_spam/ai_spam_classification/test_classification.py
+++
@@ -1,7 +0,0 @@
-import joblib
-model = joblib.load('spam_classifier_model.joblib')
-vectorizer = joblib.load('tfidf_vectorizer.joblib')
-message = input("Введите сообщение: ")
-vector = vectorizer.transform([message])
-prediction = model.predict(vector)
-print("Результат:", "Spam" if prediction[0] == 1 else "Not Spam")
